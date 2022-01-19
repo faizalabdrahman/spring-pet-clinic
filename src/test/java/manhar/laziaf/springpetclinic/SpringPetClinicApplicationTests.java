547c646945489b55e8f5f9937cc48b113554d511
faizalabdrahman <faizalabdrahman@gmail.com>
2022-01-19 23:35:10 +0800

    Initial commit

---
+++ src/test/java/manhar/laziaf/springpetclinic/SpringPetClinicApplicationTests.java
@@ -0,0 +1,15 @@
+package manhar.laziaf.springpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetClinicApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
