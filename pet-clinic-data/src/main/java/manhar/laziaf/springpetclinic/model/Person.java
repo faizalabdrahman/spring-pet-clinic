45434e04575c20de9800297d0df89efc65c31aed
faizalabdrahman <faizalabdrahman@gmail.com>
2022-01-20 09:44:11 +0800

    Implemented Base Entity Services. Closes #10

--- pet-clinic-data/src/main/java/manhar/laziaf/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/manhar/laziaf/springpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package manhar.laziaf.springpetclinic.model;
 
-public class Person
+public class Person extends BaseEntity
 {
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/manhar/laziaf/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/manhar/laziaf/springpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package manhar.laziaf.springpetclinic.model;
 
-public class PetType
+public class PetType extends BaseEntity
 {
     private String name;
 
