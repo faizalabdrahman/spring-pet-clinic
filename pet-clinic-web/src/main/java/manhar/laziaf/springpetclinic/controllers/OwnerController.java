af5bd114c6e297f5d798a0d6376b9bc4fb71f993
faizalabdrahman <faizalabdrahman@gmail.com>
2022-01-21 12:41:07 +0800

    Listed all Owners on Owners Index Page. Closes #16

--- pet-clinic-web/src/main/java/manhar/laziaf/springpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/manhar/laziaf/springpetclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package manhar.laziaf.springpetclinic.controllers;
 
+import manhar.laziaf.springpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class OwnerController
 {
-    @GetMapping({"/owners", "/owners/index", "/owners/index/html"})
-    public String listOwners()
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService)
     {
+        this.ownerService = ownerService;
+    }
+
+    @GetMapping({"/owners", "/owners/index", "/owners/index/html"})
+    public String listOwners(Model model)
+    {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
+
 }
