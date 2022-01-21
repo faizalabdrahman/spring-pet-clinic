2fb3154a99b3735b1d96f13563ee46c21f7409fd
faizalabdrahman <faizalabdrahman@gmail.com>
2022-01-21 12:51:03 +0800

    Listed all Vets on Vet Index Page. Closes #15

--- pet-clinic-web/src/main/java/manhar/laziaf/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/manhar/laziaf/springpetclinic/controllers/VetController.java
@@ -1,14 +1,25 @@
 package manhar.laziaf.springpetclinic.controllers;
 
+import manhar.laziaf.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VetController
 {
-    @GetMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets()
+    private final VetService vetService;
+
+    public VetController(VetService vetService)
     {
+        this.vetService = vetService;
+    }
+
+    @GetMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(Model model)
+    {
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
