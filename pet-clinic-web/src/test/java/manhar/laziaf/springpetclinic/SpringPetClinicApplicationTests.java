1dfea94e231f39c724344a23f7f19541cc62ae77
faizalabdrahman <faizalabdrahman@gmail.com>
2022-01-28 14:45:04 +0800

    Upgraded to JUnit 5. Closes #4

--- pet-clinic-web/src/test/java/manhar/laziaf/springpetclinic/SpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/manhar/laziaf/springpetclinic/SpringPetClinicApplicationTests.java
@@ -1,15 +1,16 @@
 package manhar.laziaf.springpetclinic;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class SpringPetClinicApplicationTests
 {
-
     @Test
     void contextLoads()
     {
     }
-
 }
