341f62cb3c4c14546d7330af87ab4a587ab625e7
faizalabdrahman <faizalabdrahman@gmail.com>
2022-02-06 11:59:03 +0800

    Implemented Vets JSON Endpoint. Closes #64

--- pet-clinic-web/src/main/java/manhar/laziaf/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/manhar/laziaf/springpetclinic/controllers/VetController.java
@@ -1,9 +1,13 @@
 package manhar.laziaf.springpetclinic.controllers;
 
+import manhar.laziaf.springpetclinic.model.Vet;
 import manhar.laziaf.springpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController
@@ -21,5 +25,11 @@ public class VetController
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson()
+    {
+        return vetService.findAll();
     }
 }
